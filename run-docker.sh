ccab975c3055769c33d25bbef6bd159c3bc438f6
talwinder1212 <115771074+talwinder1212@users.noreply.github.com>
2023-02-15 16:11:19 +0530

    update docker start script

--- run-docker.sh
+++ run-docker.sh
@@ -8,18 +8,17 @@ then
   echo "Importing database"
 fi
   while true [[ $(docker exec  demo-db  mysqladmin ping  -u root -pTest@1234 |grep "alive"  ) ==  *alive* ]]; do
-sleep 3
+
 echo "Removng Existing database"
 docker exec demo-app  php artisan  db:wipe  >/dev/null 2>&1
 if  [[ ${?} = 0 ]]
 then echo "Removed old Database"
 fi
 
-
+echo "Waiting For Database to UP"
+docker exec demo-app  php artisan  migrate --seed > /dev/null 2>&1
 if [[ ${?} = 0 ]]
 then
- echo "Importing database"
- docker exec demo-app  php artisan  migrate --seed > /dev/null 2>&1
  echo "Database Imported Sucessfully"
 exit 0
 else
